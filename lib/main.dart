8be94db64d60ffff1756656a26f5849d7c504f13
sohum <sohum1094@gmail.com>
2023-09-03 01:15:43 -0500

    Added user info popup on home screen when info is not set

--- lib/main.dart
+++ lib/main.dart
@@ -5,74 +5,43 @@ import 'existing_survey_screen.dart';
 import 'user_info/user_initial_info.dart';
 
 void main() {
-  runApp(const HomeScreen());
+  runApp(const MyApp());
 }
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // Fetch the boolean value from SharedPreferences
-    Future<bool> getUserInfoDialogStatus() async {
-      final prefs = await SharedPreferences.getInstance();
-      return (prefs.getString('First Name') == null ||
-          prefs.getString('First Name') == ''); // Default to true if not found
-    }
-
-    getUserInfoDialogStatus().then((shouldShowDialog) {
-      if (shouldShowDialog) {
-        debugPrint('should show triggered');
-        _showEnterUserInfoDialog(context);
-      }
-    });
-
-    return MaterialApp(
-      home: Builder(
-        builder: (context) => Scaffold(
-          appBar: AppBar(
-            leading: IconButton(
-              icon: const Icon(Icons.person),
-              onPressed: () => Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (BuildContext context) {
-                    return const UserInitialInfo();
-                  },
-                ),
-              ),
-            ),
-            title: const Text('IAQuick', textScaleFactor: 1.1),
-            backgroundColor: Colors.blueGrey,
-            centerTitle: true,
-          ),
-          body: Center(
-            child: SizedBox(
-              width: MediaQuery.of(context).size.width * .95,
-              height: MediaQuery.of(context).size.height * .5,
-              child: Column(
-                children: [
-                  Expanded(
-                    flex: 3,
-                    child: createNewSurveyButton(context),
-                  ),
-                  const Spacer(
-                    flex: 1,
-                  ),
-                  Expanded(
-                    flex: 3,
-                    child: openExistingSurveyButton(context),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ),
-      ),
+    return const MaterialApp(
+      home: HomeScreen(),
     );
   }
+}
 
-  ElevatedButton openExistingSurveyButton(BuildContext context) {
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  HomeScreenState createState() => HomeScreenState();
+}
+
+class HomeScreenState extends State<HomeScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _checkAndShowUserInfoDialog();
+  }
+
+  Future<void> _checkAndShowUserInfoDialog() async {
+    final shouldShowDialog = await getUserInfoDialogStatus();
+
+    if (shouldShowDialog) {
+      _showEnterUserInfoDialog(context);
+    }
+  }
+
+    ElevatedButton openExistingSurveyButton(BuildContext context) {
     return ElevatedButton(
       style: ElevatedButton.styleFrom(
         backgroundColor: Colors.indigo,
@@ -82,9 +51,7 @@ class HomeScreen extends StatelessWidget {
         context,
         MaterialPageRoute(
           builder: (BuildContext context) {
-            return ExistingSurveyScreen(
-              key: key,
-            );
+            return const ExistingSurveyScreen();
           },
         ),
       ),
@@ -102,7 +69,7 @@ class HomeScreen extends StatelessWidget {
       ),
     );
   }
-}
+
 
 ElevatedButton createNewSurveyButton(BuildContext context) {
   return ElevatedButton(
@@ -133,31 +100,156 @@ ElevatedButton createNewSurveyButton(BuildContext context) {
   );
 }
 
-void _showEnterUserInfoDialog(BuildContext context) {
-  showDialog(
-    context: context,
-    barrierDismissible: true, // Dialog can be dismissed by tapping outside
-    builder: (BuildContext context) {
-      return AlertDialog(
-        title: const Text('Welcome to IAQuick!'),
-        content: const Text('Please enter user information.'),
-        actions: <Widget>[
-          TextButton(
-            child: const Text('OK'),
-            onPressed: () {
-              Navigator.of(context).pop(); // Close the dialog
-              Navigator.of(context).push(
+  void _showEnterUserInfoDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      barrierDismissible: true,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Welcome to IAQuick!'),
+          content: const Text('Please enter user information.'),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('OK'),
+              onPressed: () {
+                Navigator.of(context).pop();
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (BuildContext context) {
+                      return const UserInitialInfo();
+                    },
+                  ),
+                );
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Future<bool> getUserInfoDialogStatus() async {
+    final prefs = await SharedPreferences.getInstance();
+    return (prefs.getString('First Name') == null ||
+        prefs.getString('First Name') == '');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+            leading: IconButton(
+              icon: const Icon(Icons.person),
+              onPressed: () => Navigator.push(
+                context,
                 MaterialPageRoute(
                   builder: (BuildContext context) {
                     return const UserInitialInfo();
                   },
                 ),
-              );
-            },
+              ),
+            ),
+            title: const Text('IAQuick', textScaleFactor: 1.1),
+            backgroundColor: Colors.blueGrey,
+            centerTitle: true,
           ),
-        ],
-      );
-    },
-  );
+      body: Center(
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width * .95,
+              height: MediaQuery.of(context).size.height * .5,
+              child: Column(
+                children: [
+                  Expanded(
+                    flex: 3,
+                    child: createNewSurveyButton(context),
+                  ),
+                  const Spacer(
+                    flex: 1,
+                  ),
+                  Expanded(
+                    flex: 3,
+                    child: openExistingSurveyButton(context),
+                  ),
+                ],
+              ),
+            ),
+          ),
+    );
+  }
 }
+
+// import 'package:flutter/material.dart';
+// import 'package:iaqapp/new_survey/new_survey_start.dart';
+// import 'package:shared_preferences/shared_preferences.dart';
+// import 'existing_survey_screen.dart';
+// import 'user_info/user_initial_info.dart';
+
+// void main() {
+//   runApp(const HomeScreen());
+// }
+
+// class HomeScreen extends StatelessWidget {
+//   const HomeScreen({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     // Fetch the boolean value from SharedPreferences
+    
+
+//     getUserInfoDialogStatus().then((shouldShowDialog) {
+//       if (shouldShowDialog) {
+//         debugPrint('should show triggered');
+//         _showEnterUserInfoDialog(context);
+//       }
+//     },
+//     );
+
+//     return MaterialApp(
+//       home: Builder(
+//         builder: (context) => Scaffold(
+//           appBar: 
+//           body: 
+//         ),
+//       ),
+//     );
+//   }
+
+
+
+
+
+// void _showEnterUserInfoDialog(BuildContext context) {
+//   showDialog(
+//     context: context,
+//     barrierDismissible: true, // Dialog can be dismissed by tapping outside
+//     builder: (BuildContext context) {
+//       return AlertDialog(
+//         title: const Text('Welcome to IAQuick!'),
+//         content: const Text('Please enter user information.'),
+//         actions: <Widget>[
+//           TextButton(
+//             child: const Text('OK'),
+//             onPressed: () {
+//               Navigator.of(context).pop(); // Close the dialog
+//               Navigator.of(context).push(
+//                 MaterialPageRoute(
+//                   builder: (BuildContext context) {
+//                     return const UserInitialInfo();
+//                   },
+//                 ),
+//               );
+//             },
+//           ),
+//         ],
+//       );
+//     },
+//   );
+// }
+
+// Future<bool> getUserInfoDialogStatus() async {
+//       final prefs = await SharedPreferences.getInstance();
+//       return (prefs.getString('First Name') == null ||
+//           prefs.getString('First Name') == ''); // Default to true if not found
+//     }
+
 
