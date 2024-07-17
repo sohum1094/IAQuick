f1ad22cbdb9f2fc271ff57e2434d4d9a9a4c1c3c
sohum <sohum1094@gmail.com>
2024-07-16 22:50:50 -0500

    fixed config to point to correct .env

--- node_backend/server/config/config.js
+++ node_backend/server/config/config.js
@@ -1,11 +1,17 @@
 import dotenv from 'dotenv';
+import path from 'path';
 
+// have to modify dotenv file path becuase the app is run from seeder folder
+dotenv.config({ path: path.join(process.cwd(), "..", ".env") })
 dotenv.config();
+
+console.log('DB_USERNAME:', process.env.DB_USERNAME); // Verify this outputs the correct value
+
 
 export default {
   port: process.env.EXPRESS_PORT || 3000,
   db: {
-    user: process.env.DB_USER || 'yourUsername',
+    user: process.env.DB_USERNAME || 'yourUsername',
     host: process.env.DB_HOST || 'localhost',
     database: process.env.DB_NAME || 'postgres',
     password: process.env.DB_PASSWORD || 'yourPassword',
