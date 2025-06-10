5f19641bebaf583c1a608212e6336bef55f9ac44
Sohum Shah <123768275+sohum1094@users.noreply.github.com>
2025-06-10 12:55:55 -0500

    Add Google sign-in and sign-up flow

--- lib/auth_service.dart
+++ lib/auth_service.dart
@@ -1,7 +1,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
 
   Stream<User?> authStateChanges() => _auth.authStateChanges();
 
@@ -15,6 +17,22 @@ class AuthService {
       await cred.user?.updateDisplayName(displayName);
     }
     return cred;
+  }
+
+  Future<UserCredential> signInWithGoogle() async {
+    final googleUser = await _googleSignIn.signIn();
+    if (googleUser == null) {
+      throw FirebaseAuthException(
+        code: 'ERROR_ABORTED_BY_USER',
+        message: 'Sign in aborted by user',
+      );
+    }
+    final googleAuth = await googleUser.authentication;
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+    return _auth.signInWithCredential(credential);
   }
 
   Future<void> signOut() => _auth.signOut();
